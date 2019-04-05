59e913430564c502fa10c9542a85d4252e2a5197
Mitnick5194 <qq519441630@gmail.com>
2019-04-06 01:50:19 +0800

    取出测试代码

--- src/main/java/com/ajie/custom/maven/plugin/util/ExecuteUtil.java
+++ src/main/java/com/ajie/custom/maven/plugin/util/ExecuteUtil.java
@@ -135,43 +135,4 @@ public final class ExecuteUtil {
 		}
 
 	}
-
-	public static void main(String[] args) {
-		try {
-			JSch jsch = new JSch();
-			Session session = jsch.getSession("", "", 22);
-			session.setPassword("");
-			session.setConfig("StrictHostKeyChecking", "no");
-			session.connect();
-			Channel channel = session.openChannel("exec");
-			((ChannelExec) channel).setCommand("/var/www/resource/test2.sh");
-			channel.setInputStream(null);
-			((ChannelExec) channel).setErrStream(System.err);
-			InputStream in = channel.getInputStream();
-			channel.connect();
-			byte[] tmp = new byte[1024];
-			while (true) {
-				while (in.available() > 0) {
-					int i = in.read(tmp, 0, 1024);
-					if (i < 0)
-						break;
-					System.out.print(new String(tmp, 0, i));
-				}
-				if (channel.isClosed()) {
-					if (in.available() > 0)
-						continue;
-					System.out.println("exit-status: " + channel.getExitStatus());
-					break;
-				}
-				try {
-					Thread.sleep(1000);
-				} catch (Exception ee) {
-				}
-			}
-			channel.disconnect();
-			session.disconnect();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
 }
